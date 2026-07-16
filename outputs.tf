c9a8d1e7e2b832e664d5fd4300716bda6d9e5406
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:46:08 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "api_management_tags_id" {
   description = "Map of id values across all api_management_tags, keyed the same as var.api_management_tags"
-  value       = { for k, v in azurerm_api_management_tag.api_management_tags : k => v.id }
+  value       = { for k, v in azurerm_api_management_tag.api_management_tags : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "api_management_tags_api_management_id" {
   description = "Map of api_management_id values across all api_management_tags, keyed the same as var.api_management_tags"
-  value       = { for k, v in azurerm_api_management_tag.api_management_tags : k => v.api_management_id }
+  value       = { for k, v in azurerm_api_management_tag.api_management_tags : k => v.api_management_id if v.api_management_id != null && length(v.api_management_id) > 0 }
 }
 output "api_management_tags_display_name" {
   description = "Map of display_name values across all api_management_tags, keyed the same as var.api_management_tags"
-  value       = { for k, v in azurerm_api_management_tag.api_management_tags : k => v.display_name }
+  value       = { for k, v in azurerm_api_management_tag.api_management_tags : k => v.display_name if v.display_name != null && length(v.display_name) > 0 }
 }
 output "api_management_tags_name" {
   description = "Map of name values across all api_management_tags, keyed the same as var.api_management_tags"
-  value       = { for k, v in azurerm_api_management_tag.api_management_tags : k => v.name }
+  value       = { for k, v in azurerm_api_management_tag.api_management_tags : k => v.name if v.name != null && length(v.name) > 0 }
 }
 
